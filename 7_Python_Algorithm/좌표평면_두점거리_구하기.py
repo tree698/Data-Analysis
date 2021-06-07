6261f9b08985ff6c50e199125ec2709c28a0f3f1
Chanwoo <tree698@gmail.com>
2021-06-07 17:21:46 +0900

    두점사이 거리

--- 7_Python_Algorithm/좌표평면_두점거리_구하기.py
+++ 7_Python_Algorithm/좌표평면_두점거리_구하기.py
@@ -7,8 +7,14 @@ b = (3,4)
 """
 a = (1,2)
 b = (3,4)
+print(type(a))
 
 result = ((3-1)**2) + ((4-2)**2)
 print(round(result**(1/2),2))
 
-print(type(a))
+def distance(a, b):
+    x_distance = abs(a[0]-b[0])**2
+    y_distance = abs(a[1]-b[1])**2
+    return round((x_distance + y_distance)**(1/2), 2)
+
+print(distance(a, b))
